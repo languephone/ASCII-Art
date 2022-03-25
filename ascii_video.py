83fb8e661e41de49fcf834dd6cb23a924036e23c
languephone <73309519+languephone@users.noreply.github.com>
2022-03-25 10:21:23 +0000

    Refactored code, changed font, define image scaling.

--- ascii_video.py
+++ ascii_video.py
@@ -2,17 +2,15 @@ import cv2
 import pygame
 import sys
 
-# TODO: Mirror-reverse images
 # TODO: Calculate screen size and crop area dynamically
 # TODO: Understand font size vertical & horizontal pixels
 
 # Image Size
-HEIGHT = int(1080 / 12)
-WIDTH = int(1920 / 10)
+VSCALE = 0.07
+HSCALE = 0.1
 
 # Colors
 BLACK = (0, 0, 0)
-GRAY = (180, 180, 180)
 WHITE = (255, 255, 255)
 
 # Fonts
@@ -27,6 +25,14 @@ division_factor = 256 / len(ascii_range)
 
 capture = cv2.VideoCapture(0)
 
+def resize_image(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    small = cv2.resize(gray, None, fx=HSCALE, fy=VSCALE,
+                       interpolation = cv2.INTER_AREA)
+
+    return small
+
+
 def convert_to_ascii(image_row):
     row_text = (''.join([ascii_reverse[int(pixel / division_factor)]
                 for pixel in row]))
@@ -34,27 +40,48 @@ def convert_to_ascii(image_row):
     return row_text
 
 
+def check_events():
+    for event in pygame.event.get():
+        # Check if game quit
+        if event.type == pygame.QUIT:
+            sys.exit()
+        # Check for pressed keys
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event)
+
+
+def check_keydown_events(event):
+    if event.key == pygame.K_UP:
+        print('up key')
+    elif event.key == pygame.K_DOWN:
+        print('down key')
+
+
 def update_screen():
     screen.fill(BLACK)
     screen.blit(ascii_text)
 
+
 # Initialize Pygame
 pygame.init()
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1100, 750))
 pygame.display.set_caption("Ascii Art")
-smallFont = pygame.font.SysFont('sfnsmono', FONT_SIZE)
+smallFont = pygame.font.SysFont('couriernew', FONT_SIZE)
 
 while True:
-    
-    # Check if game quit
+
     for event in pygame.event.get():
+        # Check if game quit
         if event.type == pygame.QUIT:
             sys.exit()
 
+    check_events()
+
     # Capture video and resize
     ret, frame = capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    small = cv2.resize(gray, (WIDTH, HEIGHT))
+
+    small = resize_image(frame)
     flipped = cv2.flip(small, 1)
     # cropped = small[60:480, 250:710]
 
@@ -63,7 +90,10 @@ while True:
     
     for index, row in enumerate(flipped):
         row_text = smallFont.render(convert_to_ascii(row), False, WHITE)
+        row_rect = row_text.get_rect()
+        pygame.draw.rect(screen, WHITE, pygame.Rect(row_rect))
         # increment y position of each text row by the # of pixels in the font
         screen.blit(row_text, (0, index * (FONT_SIZE - 1)))
 
     pygame.display.flip()
+    clock.tick(30)
