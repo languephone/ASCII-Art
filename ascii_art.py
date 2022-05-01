b819dd241fb6158b0648faf90847e3d916c384c3
languephone <73309519+languephone@users.noreply.github.com>
2022-05-01 18:37:48 +0100

    Classes for ascii_art file.

--- ascii_art.py
+++ ascii_art.py
@@ -2,20 +2,73 @@ import cv2
 import sys
 import numpy as np
 
-IMG_WIDTH = 250
-IMG_HEIGHT = IMG_WIDTH // 2
+class AsciiPhoto():
+    """Overall class to manage static photo conversion to Ascii."""
 
-img = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
-img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_AREA)
+    def __init__(self):
 
-ascii_range = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """
+        # Read image and convert to grayscale
+        self.img = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
+        
+        # Calculate output width & height
+        self.img_width, self.img_height = self.img.shape
+        img_ratio = self.img_width / self.img_height
+        self.output_width = 60
+        self.output_height = int(self.output_width / img_ratio)
 
-ascii_reverse = list(ascii_range)
-ascii_reverse.reverse()
+        # Define adjustment needed for black space between rows
+        self.line_height_adjustment = 2
 
-# Convert 8-bit (256) colour range to ascii range 
-division_factor = 256 / len(ascii_range)
+        self.output_height = int(self.output_height / self.line_height_adjustment)
 
-for row in img:
-	print(''.join([ascii_reverse[int(pixel / division_factor)]
-		for pixel in row]))
+        # Resize image to match required output size
+        self.img = cv2.resize(self.img,
+            (self.output_width , self.output_height),
+            interpolation=cv2.INTER_AREA)
+
+        self.ascii_range = """$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.           """
+
+        self.ascii_reverse = list(self.ascii_range)
+        self.ascii_reverse.reverse()
+
+        # Convert 8-bit (256) greyscale range to ascii range 
+        self.division_factor = 256 / len(self.ascii_range)
+
+
+    def print_to_file(self):
+        
+        # Print to text file
+        output_name = sys.argv[1].split('/')[-1]
+        output_name = output_name.split('.')[0] + '.txt'
+
+        with open(output_name, 'w') as f:
+            for row in self.img:
+                f.write(''.join(
+                    [self.ascii_range[int(pixel / self.division_factor)
+                        ] for pixel in row]) + '\n')
+
+
+    def print_to_terminal(self):
+        # Show in terminal
+        for row in self.img:
+            print(''.join([self.ascii_reverse[int(pixel / self.division_factor)]
+                for pixel in row]))
+
+
+    def _calc_pixel_size(self):
+        """Use window size and font size to calculate chars needed."""
+    
+        # Calculate size in pixels of chars based on font
+        line_width, self.line_height = self.font.size('#' * 20)
+        self.line_height -= self.line_height_adjustment
+        self.char_width = line_width / 20
+        # To test line hight, use chr(9608), which is a full height character
+
+        # Define number of text characters to fit on screen
+        self.h_chars = int(self.win_width / self.char_width)
+        self.v_chars = int(self.win_height / self.line_height)
+
+
+photo = AsciiPhoto()
+photo.print_to_terminal()
+photo.print_to_file()
